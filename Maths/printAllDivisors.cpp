edb49bb775d27949a398baf88908a3b6e5c0b43c
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-07-05 15:48:43 +0530

    Print all Divisors 2

--- Maths/printAllDivisors.cpp
+++ Maths/printAllDivisors.cpp
@@ -15,13 +15,28 @@ void printAllDivisors(int n)
     cout << endl;
 }
 
+void printAllDivisors2(int n)
+{
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " ";
+            if (i != n / i) // To avoid printing the square root twice
+            {
+                cout << n / i << " ";
+            }
+        }
+    }
+}
+
 int main()
 {
     int number;
     cout << "Enter a number: ";
     cin >> number;
 
-    printAllDivisors(number);
+    printAllDivisors2(number);
 
     return 0;
 }
