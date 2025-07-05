9d2462d2ea63a47f6d569b842928080a5baf9ca8
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-07-05 15:57:00 +0530

    Check Prime 2

--- Maths/checkPrime.cpp
+++ Maths/checkPrime.cpp
@@ -20,13 +20,31 @@ bool checkPrime(int n)
     return true; // If exactly 2 divisors found, it's prime
 }
 
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
 int main()
 {
     int number;
     cout << "Enter a number: ";
     cin >> number;
 
-    if (checkPrime(number))
+    if (isPrime(number))
     {
         cout << number << " is a prime number." << endl;
     }
