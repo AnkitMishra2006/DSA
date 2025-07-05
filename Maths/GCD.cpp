c1b93a6273f5fa1f09697e6b185c53a92496649e
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-07-05 20:17:27 +0530

    GCD of 2 numbers 2

--- Maths/GCD.cpp
+++ Maths/GCD.cpp
@@ -14,11 +14,25 @@ int gcd(int a, int b)
     return gcd;
 }
 
+int gcd2(int a, int b)
+{
+    int gcd = 1;
+    for (int i = min(a, b); i >= 1; i--)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+            break;
+        }
+    }
+    return gcd;
+}
+
 int main()
 {
     int a, b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
-    cout << "GCD of " << a << " and " << b << " is: " << gcd(a, b) << endl;
+    cout << "GCD of " << a << " and " << b << " is: " << gcd2(a, b) << endl;
     return 0;
 }
