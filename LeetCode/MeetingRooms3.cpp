8cece1c9a8028be740300b755dd6ef39b1069cb6
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-07-12 00:41:29 +0530

    2402. Meeting Rooms 3 LC - Hard Optimal Solution

--- LeetCode/MeetingRooms3.cpp
+++ LeetCode/MeetingRooms3.cpp
@@ -3,6 +3,7 @@
 #include <climits>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int mostBooked(int n, vector<vector<int>> &meetings)
@@ -58,6 +59,66 @@ int mostBooked(int n, vector<vector<int>> &meetings)
     }
     return resultRoom;
 }
+
+typedef pair<long long, int> P;
+int mostBooked(int n, vector<vector<int>> &meetings)
+{
+    int m = meetings.size();
+    sort(meetings.begin(),
+         meetings.end()); // Sort by Starting Time of the Meeting
+    vector<int> roomsUsedCount(
+        n, 0); // Stores count of the meetings in each room
+
+    priority_queue<P, vector<P>, greater<P>> usedRooms;
+    priority_queue<int, vector<int>, greater<int>> availableRooms;
+
+    for (int room = 0; room < n; room++)
+    {
+        availableRooms.push(room);
+    }
+    for (vector<int> &meet : meetings)
+    {
+        int start = meet[0];
+        int end = meet[1];
+        int duration = end - start;
+        while (!usedRooms.empty() && usedRooms.top().first <= start)
+        {
+            int room = usedRooms.top().second;
+            usedRooms.pop();
+            availableRooms.push(room);
+        }
+
+        if (!availableRooms.empty())
+        {
+            int room = availableRooms.top();
+            availableRooms.pop();
+            usedRooms.push({end, room});
+            roomsUsedCount[room]++;
+        }
+        else
+        {
+            int room = usedRooms.top().second;
+            long long endTime = usedRooms.top().first;
+
+            usedRooms.pop();
+            usedRooms.push({endTime + duration, room});
+            roomsUsedCount[room]++;
+        }
+    }
+
+    int resultRoom = -1;
+    int maxUse = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (roomsUsedCount[i] > maxUse)
+        {
+            maxUse = roomsUsedCount[i];
+            resultRoom = i;
+        }
+    }
+    return resultRoom;
+}
+
 int main()
 {
     int n = 2;
